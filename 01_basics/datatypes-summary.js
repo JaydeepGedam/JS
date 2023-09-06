cbd9cd857e7f1e2bbcc9583d66f516ba5014f3ea
JaydeepGedam <95856205+JaydeepGedam@users.noreply.github.com>
2023-09-06 18:23:03 +0000

    added concept of stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -36,3 +36,36 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Stack and Heap memory in javascript
+
+let myName = "Jaydeep"
+let anotherName = myName
+anotherName = "JD"
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    email : "jd@gmail.com",
+    age : 20,
+}
+
+let userTwo = userOne
+
+userTwo.email = "jaydeep@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+/*Primitive dataType goes in stack memory; And it provides copy of that element.
+So when we change something (through assigned variable) it gets changed in copy of element  not in original.
+
+
+Opposite of this. Non primitive dataType get stored in HEAP memory. 
+And it gives reference value to assigned elements. 
+So when assigned variable makes changes it changes the both values(original and assigned one. */
+
